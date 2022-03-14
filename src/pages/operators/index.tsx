aed020f5423d617372cfd8e32fc5fa5412eedd47
Илья Оловянников <ilaolovannikov@MacBook-Pro-Ila>
2022-03-14 16:39:57 +0300

    ssr

--- src/pages/operators/index.tsx
+++ src/pages/operators/index.tsx
@@ -1,13 +1,14 @@
 import { Operators } from '@/public-app';
 import { getData } from '@client/services/getData';
-import {IPageData} from '@/types/index';
+import { IPageData } from '@/types/index';
+import { GetStaticProps } from 'next';
 
-const OperatorsPage = ({data}: IPageData): JSX.Element => <Operators data={data} />;
+const OperatorsPage = ({ data }: IPageData): JSX.Element => <Operators data={data} />;
 
 export default OperatorsPage;
 
-export const getStaticProps = async () => {
-    const data = await getData(`${process.env.API_HOST}/content/operators/operators.json`);
+export const getServerSideProps: GetStaticProps = async () => {
+    const data = await getData(`${process.env.API_HOST}/data/content/operators/operators.json`);
 
     if (!data) return { notFound: true };
 
